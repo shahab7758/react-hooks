f53eca3743306f6f870bb6b967f375eb19abd7ae
shahab7758 <shahabm298@gmail.com>
2020-08-30 03:28:40 +0500

    added reset button

--- src/HookCountertwo.jsx
+++ src/HookCountertwo.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const HookCountertwo = () => {
-    const initialCount=0
-    const [count, setcount] = useState(initialCount);
-  const  incrementByFive=()=>{
-        for(let i=0; i<5;i++){
-            setcount(preCount=>preCount+1)
-        }
+  const initialCount = 0;
+  const [count, setcount] = useState(initialCount);
+  const incrementByFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setcount((preCount) => preCount + 1);
     }
-    return ( 
-        <div>
-            Count : {count}
-            <button onClick={()=>setcount(count+1)}>Increment</button>
-            <button onClick={() => {
-                setcount(count-1)
-            }}>Decrement</button>
-            <button onClick={incrementByFive}>Increment by 5</button>
-        </div>
-     );
-}
- 
+  };
+  return (
+    <div>
+      Count : {count}
+      <button onClick={() => setcount(count + 1)}>Increment</button>
+      <button
+        onClick={() => {
+          setcount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          setcount(initialCount);
+        }}
+      >
+        Reset
+      </button>
+      <button onClick={incrementByFive}>Increment by 5</button>
+    </div>
+  );
+};
+
 export default HookCountertwo;
