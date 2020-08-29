353b993a26ef2f634c43f4f540b7ce173ec8f639
shahab7758 <shahabm298@gmail.com>
2020-08-29 11:26:36 -0400

    Added App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ClassCount from './ClassCounter';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassCount/>
+    </div>
+  );
+}
+
+export default App;
