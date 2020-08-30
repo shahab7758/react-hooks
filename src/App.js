2985a2fec9b83b05f9119c82dad77249f0547518
shahab7758 <shahabm298@gmail.com>
2020-08-30 22:05:21 +0500

    Complete concept of hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import ClassCount from './ClassCounter';
+import React from "react";
+import "./App.css";
+// import ClassCounterTitle from "./ClassCounterTitle";
+// import HookCounteruseEffect from "./HookCounteruseEffect";
+import MovemouseContainer from "./Movemousec/MovemouseContainer";
+// import ClassCount from "./ClassCounter";
+// import HookCounter from "./HookCounter";
+// import HookCountertwo from "./HookCountertwo";
+// import HookForm from "./HookForm";
+// import HookArray from "./HookArray";
 
 function App() {
   return (
     <div className="App">
-     <ClassCount/>
+      {/* <ClassCount/> */}
+      {/* <HookCounter/> */}
+      {/*  <HookCountertwo />
+      <HookForm />
+      <HookArray />
+      <ClassCounterTitle />
+      <HookCounteruseEffect />*/}
+      <MovemouseContainer />
     </div>
   );
 }
