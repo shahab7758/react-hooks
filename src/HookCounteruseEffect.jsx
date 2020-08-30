34f032e2251c33dca22befd45d38b6c818df9b35
shahab7758 <shahabm298@gmail.com>
2020-08-30 18:06:09 +0500

    prevent un neccessary rendering

--- src/HookCounteruseEffect.jsx
+++ src/HookCounteruseEffect.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 const HookCounteruseEffect = () => {
   const [count, setcount] = useState(0);
+  const [name, setname] = useState("");
   useEffect(() => {
+    console.log("rendered");
     document.title = `Clicked ${count} times`;
-  });
+  }, [count]);
   return (
-    <button
-      onClick={() => {
-        setcount(count + 1);
-      }}
-    >
-      You have Clicked {count} times
-    </button>
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setcount(count + 1);
+        }}
+      >
+        You have Clicked {count} times
+      </button>
+    </div>
   );
 };
 
